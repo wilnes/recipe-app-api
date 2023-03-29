ad9aa8daa812f5e6922d8669d19c4d3efd2c1def
wilnes <wilfrieddjahou@gmail.com>
2023-03-29 11:38:26 +0200

    Add actions.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """ Add x to y and return result."""
+    return x+y
+
+def substract(x,y):
+    """Substract x from y and return result."""
+    return y-x
